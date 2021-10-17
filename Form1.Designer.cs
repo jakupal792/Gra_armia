596ca3deb284329be378ed0a6982e718c5d5e3f6
Jakub Palka <jakupal792@student.polsl.pl>
2021-10-17 18:46:11 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSBomby = new System.Windows.Forms.TextBox();
+            this.txtSMoc = new System.Windows.Forms.TextBox();
+            this.txtSNazwa = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstPojazdy = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKaliber = new System.Windows.Forms.TextBox();
+            this.txtMoc = new System.Windows.Forms.TextBox();
+            this.txtNazwa = new System.Windows.Forms.TextBox();
+            this.btnCreateTank = new System.Windows.Forms.Button();
+            this.btnCreatePlane = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPulap = new System.Windows.Forms.TextBox();
+            this.btnFigth = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtID1 = new System.Windows.Forms.TextBox();
+            this.txtID2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnRandomFigth = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(285, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 17);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Moc";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(283, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 34);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Ładunek\r\nbomb";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(285, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 17);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Nazwa";
+            // 
+            // txtSBomby
+            // 
+            this.txtSBomby.Location = new System.Drawing.Point(355, 189);
+            this.txtSBomby.Name = "txtSBomby";
+            this.txtSBomby.Size = new System.Drawing.Size(100, 22);
+            this.txtSBomby.TabIndex = 28;
+            // 
+            // txtSMoc
+            // 
+            this.txtSMoc.Location = new System.Drawing.Point(355, 137);
+            this.txtSMoc.Name = "txtSMoc";
+            this.txtSMoc.Size = new System.Drawing.Size(100, 22);
+            this.txtSMoc.TabIndex = 27;
+            // 
+            // txtSNazwa
+            // 
+            this.txtSNazwa.Location = new System.Drawing.Point(355, 87);
+            this.txtSNazwa.Name = "txtSNazwa";
+            this.txtSNazwa.Size = new System.Drawing.Size(100, 22);
+            this.txtSNazwa.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(210, 290);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 17);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Pojazdy";
+            // 
+            // lstPojazdy
+            // 
+            this.lstPojazdy.FormattingEnabled = true;
+            this.lstPojazdy.ItemHeight = 16;
+            this.lstPojazdy.Location = new System.Drawing.Point(50, 328);
+            this.lstPojazdy.Name = "lstPojazdy";
+            this.lstPojazdy.Size = new System.Drawing.Size(392, 132);
+            this.lstPojazdy.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Moc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Kaliber";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Nazwa";
+            // 
+            // txtKaliber
+            // 
+            this.txtKaliber.Location = new System.Drawing.Point(101, 244);
+            this.txtKaliber.Name = "txtKaliber";
+            this.txtKaliber.Size = new System.Drawing.Size(100, 22);
+            this.txtKaliber.TabIndex = 19;
+            // 
+            // txtMoc
+            // 
+            this.txtMoc.Location = new System.Drawing.Point(101, 172);
+            this.txtMoc.Name = "txtMoc";
+            this.txtMoc.Size = new System.Drawing.Size(100, 22);
+            this.txtMoc.TabIndex = 18;
+            // 
+            // txtNazwa
+            // 
+            this.txtNazwa.Location = new System.Drawing.Point(101, 92);
+            this.txtNazwa.Name = "txtNazwa";
+            this.txtNazwa.Size = new System.Drawing.Size(100, 22);
+            this.txtNazwa.TabIndex = 17;
+            // 
+            // btnCreateTank
+            // 
+            this.btnCreateTank.Location = new System.Drawing.Point(50, 12);
+            this.btnCreateTank.Name = "btnCreateTank";
+            this.btnCreateTank.Size = new System.Drawing.Size(133, 42);
+            this.btnCreateTank.TabIndex = 16;
+            this.btnCreateTank.Text = "Utworz czolg";
+            this.btnCreateTank.UseVisualStyleBackColor = true;
+            this.btnCreateTank.Click += new System.EventHandler(this.btnCreateTank_Click);
+            // 
+            // btnCreatePlane
+            // 
+            this.btnCreatePlane.Location = new System.Drawing.Point(311, 12);
+            this.btnCreatePlane.Name = "btnCreatePlane";
+            this.btnCreatePlane.Size = new System.Drawing.Size(133, 42);
+            this.btnCreatePlane.TabIndex = 32;
+            this.btnCreatePlane.Text = "Utworz samolot";
+            this.btnCreatePlane.UseVisualStyleBackColor = true;
+            this.btnCreatePlane.Click += new System.EventHandler(this.btnCreatePlane_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(285, 244);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 17);
+            this.label8.TabIndex = 34;
+            this.label8.Text = "Pułap";
+            // 
+            // txtPulap
+            // 
+            this.txtPulap.Location = new System.Drawing.Point(357, 244);
+            this.txtPulap.Name = "txtPulap";
+            this.txtPulap.Size = new System.Drawing.Size(100, 22);
+            this.txtPulap.TabIndex = 33;
+            // 
+            // btnFigth
+            // 
+            this.btnFigth.Location = new System.Drawing.Point(575, 55);
+            this.btnFigth.Name = "btnFigth";
+            this.btnFigth.Size = new System.Drawing.Size(180, 75);
+            this.btnFigth.TabIndex = 35;
+            this.btnFigth.Text = "Walka";
+            this.btnFigth.UseVisualStyleBackColor = true;
+            this.btnFigth.Click += new System.EventHandler(this.btnFigth_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(572, 191);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 17);
+            this.label9.TabIndex = 36;
+            this.label9.Text = "Id pojazdu 1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(572, 248);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(85, 17);
+            this.label10.TabIndex = 37;
+            this.label10.Text = "Id pojazdu 2";
+            // 
+            // txtID1
+            // 
+            this.txtID1.Location = new System.Drawing.Point(682, 188);
+            this.txtID1.Name = "txtID1";
+            this.txtID1.Size = new System.Drawing.Size(66, 22);
+            this.txtID1.TabIndex = 38;
+            // 
+            // txtID2
+            // 
+            this.txtID2.Location = new System.Drawing.Point(682, 248);
+            this.txtID2.Name = "txtID2";
+            this.txtID2.Size = new System.Drawing.Size(66, 22);
+            this.txtID2.TabIndex = 39;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(500, 328);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(311, 22);
+            this.txtResult.TabIndex = 40;
+            // 
+            // btnRandomFigth
+            // 
+            this.btnRandomFigth.Location = new System.Drawing.Point(585, 401);
+            this.btnRandomFigth.Name = "btnRandomFigth";
+            this.btnRandomFigth.Size = new System.Drawing.Size(157, 42);
+            this.btnRandomFigth.TabIndex = 41;
+            this.btnRandomFigth.Text = "Walka losowa";
+            this.btnRandomFigth.UseVisualStyleBackColor = true;
+            this.btnRandomFigth.Click += new System.EventHandler(this.btnRandomFigth_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(846, 477);
+            this.Controls.Add(this.btnRandomFigth);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtID2);
+            this.Controls.Add(this.txtID1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnFigth);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtPulap);
+            this.Controls.Add(this.btnCreatePlane);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtSBomby);
+            this.Controls.Add(this.txtSMoc);
+            this.Controls.Add(this.txtSNazwa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lstPojazdy);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKaliber);
+            this.Controls.Add(this.txtMoc);
+            this.Controls.Add(this.txtNazwa);
+            this.Controls.Add(this.btnCreateTank);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSBomby;
+        private System.Windows.Forms.TextBox txtSMoc;
+        private System.Windows.Forms.TextBox txtSNazwa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstPojazdy;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKaliber;
+        private System.Windows.Forms.TextBox txtMoc;
+        private System.Windows.Forms.TextBox txtNazwa;
+        private System.Windows.Forms.Button btnCreateTank;
+        private System.Windows.Forms.Button btnCreatePlane;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPulap;
+        private System.Windows.Forms.Button btnFigth;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtID1;
+        private System.Windows.Forms.TextBox txtID2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnRandomFigth;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        List<Pojazd> Lista_pojazdow;
+        public Form1()
+        {
+            InitializeComponent();
+            Lista_pojazdow = new List<Pojazd>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public abstract class Pojazd
+        {
+            protected static int Ilosc = 0;
+
+            protected int _id;
+            public int Id { get => _id; set => _id = value; }
+
+            protected string _nazwa;
+            public string Nazwa { get => _nazwa; set => _nazwa = value; }
+
+            protected double _moc;
+            public double Moc { get => _moc; set => _moc = value; }
+
+            public Pojazd(string nazwa, double moc)
+            {
+                Ilosc++;
+                Id = Ilosc;
+                Nazwa = nazwa;
+                Moc = moc;
+            }
+
+            public virtual string Wypisz()
+            {
+                return " - " + this.Nazwa + "; moc: " + this.Moc;
+            }
+        }
+
+        public class Czolg : Pojazd
+        {
+            protected double _kaliber;
+            public double Kaliber { get=>_kaliber; set=>_kaliber=value; }
+
+            public Czolg(string nazwa, double moc, double kaliber) : base(nazwa, moc)
+            {
+                Kaliber = kaliber;
+            }
+
+            public override string Wypisz()
+            {
+                return this.Id + ". Czolg " + base.Wypisz() + "; kaliber: " + this.Kaliber;
+            }
+        }
+
+        public class Samolot : Pojazd
+        {
+            protected double _bomby;
+            public double Bomby { get=>_bomby; set=>_bomby=value; }
+
+            protected double _pulap;
+            public double Pulap { get=>_pulap; set=>_pulap=value; }
+
+            public Samolot(string nazwa, double moc, double bomby, double pulap) : base(nazwa, moc)
+            {
+                Bomby = bomby;
+                Pulap = pulap;
+            }
+
+            public override string Wypisz()
+            {
+                return this.Id + ". Samolot " + base.Wypisz() + "; ładunek bomb: " + this.Bomby + "; pulap: " + this.Pulap;
+            }
+        }
+
+        private void btnCreateTank_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nazwa = txtNazwa.Text;
+                if (String.IsNullOrWhiteSpace(nazwa)) throw new Exception();
+                double moc = double.Parse(txtMoc.Text);
+                double kaliber = double.Parse(txtKaliber.Text);
+                if (moc <= 0 || kaliber <= 0) throw new Exception();
+                Czolg c = new Czolg(nazwa, moc, kaliber);
+                Lista_pojazdow.Add(c);
+                Wyswietl_pojazdy();
+
+            }
+            catch (Exception h)
+            {
+                MessageBox.Show("Wprowadź poprawne dane!");
+            }
+       
+        }
+
+        private void btnCreatePlane_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                string nazwa = txtSNazwa.Text;
+                if (String.IsNullOrWhiteSpace(nazwa)) throw new Exception();
+                double moc = double.Parse(txtSMoc.Text);
+                double bomby = double.Parse(txtSBomby.Text);
+                double pulap = double.Parse(txtPulap.Text);
+                if (moc <= 0 || bomby <= 0 || pulap <=0) throw new Exception();
+                Samolot c = new Samolot(nazwa, moc, bomby, pulap);
+                Lista_pojazdow.Add(c);
+                Wyswietl_pojazdy();
+            }
+            catch(Exception h)
+            {
+                MessageBox.Show("Wprowadź poprawne dane!");
+            }
+        }
+
+        private void Wyswietl_pojazdy()
+        {
+            lstPojazdy.Items.Clear();
+            foreach (Pojazd p in Lista_pojazdow)
+            {
+                lstPojazdy.Items.Add(p.Wypisz());
+            }
+        }
+
+        private void btnFigth_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id1 = int.Parse(txtID1.Text);
+                int id2 = int.Parse(txtID2.Text);
+                Fight(id1, id2);
+            }
+            catch (Exception h)
+            {
+                MessageBox.Show("Wprowadź id istniejących pojazdów!");
+            }
+        }
+
+        private void btnRandomFigth_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Random r = new Random();
+                int max = Lista_pojazdow.Count + 1;
+                Fight(r.Next(1, max), r.Next(1, max));
+            }
+            catch (Exception h)
+            {
+                MessageBox.Show("Wprowadź id istniejących pojazdów!");
+            }
+        }
+
+        private void Fight(int id1, int id2)
+        {
+            var p1 = Lista_pojazdow.Find(p => p.Id == id1);
+            var p2 = Lista_pojazdow.Find(p => p.Id == id2);
+
+            if (p1 is Czolg && p2 is Czolg)
+            {
+                Czolg c1 = p1 as Czolg;
+                Czolg c2 = p2 as Czolg;
+
+                if (c1.Kaliber > c2.Kaliber) txtResult.Text = opis_walki(id1, id2, id1);
+                else if (c1.Kaliber < c2.Kaliber) txtResult.Text = opis_walki(id1, id2, id2);
+                else if (c1.Kaliber == c2.Kaliber)
+                {
+                    if (c1.Moc > c2.Moc) txtResult.Text = opis_walki(id1, id2, id1);
+                    else if (c1.Moc < c2.Moc) txtResult.Text = opis_walki(id1, id2, id2);
+                    else if (c1.Moc == c2.Moc) txtResult.Text = opis_walki(id1, id2);
+                }
+            }
+            else if (p1 is Samolot && p2 is Samolot)
+            {
+                Samolot s1 = p1 as Samolot;
+                Samolot s2 = p2 as Samolot;
+
+                if (s1.Pulap > s2.Pulap) txtResult.Text = opis_walki(id1, id2, id1);
+                else if (s1.Pulap < s2.Pulap) txtResult.Text = opis_walki(id1, id2, id2);
+                else if (s1.Pulap == s2.Pulap)
+                {
+                    if (s1.Moc > s2.Moc) txtResult.Text = opis_walki(id1, id2, id1);
+                    else if (s1.Moc < s2.Moc) txtResult.Text = opis_walki(id1, id2, id2);
+                    else if (s1.Moc == s2.Moc) txtResult.Text = opis_walki(id1, id2);
+                }
+            }
+            else
+            {
+                Samolot s;
+                Czolg c;
+                if (p1 is Samolot)
+                {
+                    s = p1 as Samolot;
+                    c = p2 as Czolg;
+                }
+                else
+                {
+                    c = p1 as Czolg;
+                    s = p2 as Samolot;
+                }
+
+                if (s.Bomby > c.Kaliber) txtResult.Text = opis_walki(s.Id, c.Id, s.Id);
+                else if (s.Bomby < c.Kaliber) txtResult.Text = opis_walki(s.Id, c.Id, c.Id);
+                else if (s.Bomby == c.Kaliber)
+                {
+                    if (s.Moc > c.Moc) txtResult.Text = opis_walki(s.Id, c.Id, s.Id);
+                    else if (s.Moc < c.Moc) txtResult.Text = opis_walki(s.Id, c.Id, c.Id);
+                    else if (s.Moc == c.Moc) txtResult.Text = opis_walki(s.Id, c.Id);
+                }
+            }
+        }
+
+        private string opis_walki(int id1, int id2, int idZw)
+        {
+            var p1 = Lista_pojazdow.Find(p => p.Id == id1);
+            var p2 = Lista_pojazdow.Find(p => p.Id == id2);
+            var zw = Lista_pojazdow.Find(p => p.Id == idZw);
+            return (p1.GetType().Name).ToString() + " " + p1.Nazwa + " vs " + (p2.GetType().Name).ToString() + " " + p2.Nazwa + ": Wygrał " + (zw.GetType().Name).ToString() + " " + zw.Nazwa;
+        }
+
+        private string opis_walki(int id1, int id2)
+        {
+            var p1 = Lista_pojazdow.Find(p => p.Id == id1);
+            var p2 = Lista_pojazdow.Find(p => p.Id == id2);
+            return (p1.GetType().Name).ToString() + " " + p1.Nazwa + " vs " + (p2.GetType().Name).ToString() + " " + p2.Nazwa + ": REMIS";
+        }
+    }
+}
